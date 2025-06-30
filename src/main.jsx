517695158d36dc44d0bbe688d150a2cc69331e04
zain <zainjaved@codekhalaq.com>
2025-06-30 12:45:38 +0500

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 import './style.css'
 
@@ -10,7 +11,8 @@ if (!rootElement) {
 }
 
 ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+    <Analytics />
+  </>
 )
